06d2076b0b67944305faa117a51380ff0da2e5a3
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2011-11-14 00:27:34 -0800

    First commit

---
+++ sub/heroku/colors.py
@@ -0,0 +1,45 @@
+from itty import *
+import tropo
+import redis
+
+
+@get('/')
+def webpage(_):
+
+    print 'hello'
+    return serve_static_file(_, 'colors.html', root)
+
+
+@get('/color')
+def color(_):
+
+    store = get_redis_ready()
+    color = store.get('color') or 'white'
+    return color
+
+
+@post('/change_color/(.*)')
+def change_color(_, color):
+
+    phone = get_tropo_ready()
+    store = get_redis_ready()
+    store.set('color', color)
+
+
+def get_tropo_ready():
+    """Get the phone service ready."""
+    return tropo.Tropo
+
+
+def get_redis_ready():
+    """Get the variable bucket ready."""
+    store = redis.Redis(
+        host='tetra.redistogo.com',
+        password='a0577641ea43385552c5a1cdf120d437',
+        port=9463,
+        db=0)
+    return store
+
+
+root = os.path.dirname(__file__)
+run_itty()
